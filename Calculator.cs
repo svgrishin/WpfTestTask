535d667d3e9f430194bd9cf488bbf1a013b7f06b
svgrishin <dr_grishin_91@mail.ru>
2022-03-16 21:59:17 +0300

    Перенос класса калькулятора и функций формы

--- Calculator.cs
+++ Calculator.cs
@@ -6,7 +6,388 @@ using System.Threading.Tasks;
 
 namespace WpfTestTask
 {
-    internal class Calculator
+    public class Calculator
     {
+        /// <summary>
+        /// Флаг нажатия функции
+        /// </summary>
+        public bool funcFlag = false;
+        /// <summary>
+        /// Флаг нажатия результата
+        /// </summary>
+        public bool resBtnFlag = false;
+        /// <summary>
+        /// Флаг задействования памяти
+        /// </summary>
+        public bool mrFlag = false;
+
+
+        /// <summary>
+        /// Математический знак
+        /// </summary>
+        public string symbol;
+
+        /// <summary>
+        /// Описание делагата функций калькулятора для передачи в процедуру вычисления
+        /// <param name="a">Функция вычисления</param>
+        /// </summary>
+        public delegate double funcDeleg(double[] a);
+
+        /// <summary>
+        /// Делегат функций
+        /// </summary>
+        public funcDeleg fDeleg;
+
+        /// <summary>
+        /// Массив с аргументами вычислений
+        /// </summary>
+        public double[] args = new double[2];
+
+        /// <summary>
+        /// Общий вид вычисления
+        /// </summary>
+        public string resultString;
+
+        /// <summary>
+        /// Определяет, к какому из двух арументов будет обращение
+        /// </summary>
+        public bool index;
+
+        /// <summary>
+        /// Строка с числом в процессе набора
+        /// </summary>
+        public string arg;
+
+        /// <summary>
+        /// Содержимое дисплея
+        /// </summary>
+        public string disp;
+
+        /// <summary>
+        /// Метка отрицательности для агрумента
+        /// </summary>
+        public bool minus;
+
+        /// <summary>
+        /// Массив памяти
+        /// </summary>
+        public double[] mr;
+
+        /// <summary>
+        /// Массив строк формата json для загрузки/сохранения состояний
+        /// </summary>
+        public string[] jsonString;
+
+        /// <summary>
+        /// Класс функций вычисления
+        /// </summary>
+        public class CalcFunction
+        {
+            public double Summ(double[] a)
+            {
+                return a[0] + a[1];
+            }
+
+            public double multiply(double[] a)
+            {
+                return a[0] * a[1];
+            }
+
+            public double divide(double[] a)
+            {
+                return a[0] / a[1];
+            }
+
+            public double differens(double[] a)
+            {
+                return a[0] - a[1];
+            }
+
+            public double sqrOf(double[] a)
+            {
+                return Math.Pow(a[0], 2);
+            }
+
+            public double sqrtOf(double[] a)
+            {
+                return Math.Sqrt(a[0]);
+            }
+
+            public double fraction(double[] a)
+            {
+                return 1 / a[0];
+            }
+        }
+
+        /// <summary>
+        /// Конструктор для клонирования калькулятора
+        /// </summary>
+        /// <param name="c">Исходный калькулятор</param>
+        /// <returns>Клон калькулятора</returns>
+        public Calculator(Calculator c)
+        {
+            resBtnFlag = c.resBtnFlag;
+            funcFlag = c.funcFlag;
+            mrFlag = c.mrFlag;
+
+            symbol = c.symbol;
+
+            args = new double[2];
+            args[0] = c.args[0];
+            args[1] = c.args[1]; ;
+
+            resultString = c.resultString;
+
+            index = true;
+            funcFlag = false;
+
+            arg = args[0].ToString();
+            disp = c.disp;
+            minus = c.minus;
+
+            mr = c.mr;
+
+            jsonString = c.jsonString;
+        }
+
+        /// <summary>
+        /// Конструктор для калькулятора по умолчанию
+        /// </summary>
+        /// <returns>Калькулятор</returns>
+        public Calculator()
+        {
+            index = false;
+            arg = "";
+            disp = "0";
+            minus = false;
+
+            mr = new double[1];
+
+            jsonString = new string[1];
+
+            fDeleg = null;
+
+            args = new double[2];
+        }
+
+        /// <summary>
+        /// Сброс состояния калькулятора
+        /// </summary>
+        public void ResetCalc()
+        {
+            resBtnFlag = false;
+            funcFlag = false;
+            mrFlag = false;
+
+            arg = "";
+            disp = "0";
+            index = false;
+            minus = false;
+            fDeleg = null;
+        }
+
+        /// <summary>
+        /// Ввод символов
+        /// </summary>
+        /// <param name="c">Вводимый символ</param>
+        /// <returns>Форматированную строку для дисплея</returns>
+        public string inputValues(char c)
+        {
+            if (funcFlag == true)
+            {
+                arg = "";
+                funcFlag = false;
+            }
+
+            if (resBtnFlag == true) index = false;
+
+            funcFlag = false;
+
+            int i = 15;
+
+            if (c == '-') i = 17;
+
+            if (arg.Length <= i)
+            {
+                string s;
+
+                if (c == '-')
+                {
+                    if (arg != "")
+                    {
+                        switch (minus)
+                        {
+                            case true: arg = c + arg; break;
+                            case false: arg = arg.TrimStart(c); break;
+                        }
+                        disp = arg;
+                        s = arg;
+                    }
+                    else s = disp;
+                }
+                else
+                {
+                    arg += c;
+                    s = arg;
+                }
+                return displayOut(s);
+            }
+            else return displayOut(arg);
+        }
+
+        /// <summary>
+        /// Удаление символа
+        /// </summary>
+        /// <returns>Форматированную строку для дисплея</returns>
+        public string deleteSymbol()
+        {
+            if (arg.Length > 1 + Convert.ToInt16(minus))
+            {
+                arg = arg.Substring(0, arg.Length - 1);
+                return displayOut(arg);
+            }
+            else
+            {
+                arg = "";
+                disp = "0";
+                return displayOut(disp);
+            }
+        }
+
+        /// <summary>
+        /// Форматирование строки дисплея
+        /// </summary>
+        /// <param name="s">Неотформатированная строка для дисплея</param>
+        /// <returns>Строка для дисплея с пробелами по разрядам</returns>
+        public string displayOut(string s)
+        {
+            bool minusFlag = false;
+            if (s[0] == '-')
+            {
+                s = s.TrimStart('-');
+                minusFlag = true;
+            }
+            if (s.Contains(',') == false)
+            {
+                for (int i = 3; i <= s.Length; i += 4)
+                {
+                    s = s.Insert(s.Length - i, " ");
+                }
+            }
+            s = s.TrimStart(' ');
+            if (minusFlag == true) s = s.Insert(0, "-");
+            return s;
+        }
+
+        /// <summary>
+        /// Добавление операции к списку операций текущего сеанса
+        /// </summary>
+        /// <param name="s">Операция целиком</param>
+        private void addingCalcToList(string s)
+        {
+            if (s == "√" || s == "1/")
+            {
+                addToCalcString(symbol);
+                addToCalcString(args[0]);
+                addToCalcString("=");
+            }
+            else
+            {
+                addToCalcString(args[0]);
+                addToCalcString(symbol);
+                addToCalcString(args[1]);
+                addToCalcString("=");
+            }
+        }
+
+        /// <summary>
+        /// Получение результата
+        /// </summary>
+        /// <param name="cf">Делагат функции вычисления результата</param>
+        public void getResult(funcDeleg cf)
+        {
+            try
+            {
+                addingCalcToList(symbol);
+            }
+            catch { }
+
+            try
+            {
+                args[0] = cf(args);
+            }
+            catch
+            {
+                args[0] = fDeleg(args);
+            }
+
+            fDeleg = cf;
+            addToCalcString(args[0]);
+
+            disp = displayOut(Convert.ToString(args[0]));
+        }
+
+        /// <summary>
+        /// Вычисление функций одного арумента
+        /// </summary>
+        /// <param name="cf">Делагат функции вычисления результата</param>
+        public void extraFunc(funcDeleg cf)
+        {
+            //функция для одного аргумента
+            //особенность в том, что может быть успешно выполнена 
+            //по упрощённому алгоритму
+
+            tryToGetArg(arg);
+            args[1] = 2;
+
+            getResult(cf);
+
+            arg = "";
+            index = !index;
+        }
+
+        /// <summary>
+        /// Присвоение аргументу значения
+        /// </summary>
+        /// <param name="s">Строка со значением для аргумента</param>
+        public void tryToGetArg(string s)
+        {
+            try
+            {
+                args[Convert.ToByte(index)] = Convert.ToDouble(s);
+                arg = "";
+            }
+            catch
+            {
+                if (resBtnFlag == false)
+                {
+                    index = true;
+                    args[1] = args[0];
+                    resBtnFlag = true;
+                }
+            }
+            index = true;
+            disp = s;
+        }
+
+        /// <summary>
+        /// Построение строки с вычислением целиком
+        /// </summary>
+        /// <param name="s">Добавляемая часть к строке вычисления</param>
+        /// <overloads>Принимает для добавления как строку, так и число</overloads>
+        private void addToCalcString(string s)
+        {
+            resultString = string.Concat(resultString, s);
+        }
+
+        /// <summary>
+        /// Построение строки с вычислением целиком
+        /// </summary>
+        /// <param name="s">Число для добавления к строке вычисления</param>
+        /// <overloads>Принимает для добавления как строку, так и число</overloads>
+        private void addToCalcString(double s)
+        {
+            resultString = string.Concat(resultString, s.ToString());
+        }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace WpfTestTask
 {
@@ -20,15 +22,94 @@ namespace WpfTestTask
     /// </summary>
     public partial class MainWindow : Window
     {
-        #pragma warning disable IDE0044 // Добавить модификатор только для чтения
-        private readonly MRForm mrForm;
-        private readonly HistoryForm HF;
         bool sizeFlag = false;
+
+        /// <summary>
+        /// Объект калькулятора
+        /// </summary>
+        public Calculator calc = new Calculator();
+
+        /// <summary>
+        /// Массив с калькуляторами для хранения объектов с различными состояниями
+        /// </summary>
+        public Calculator[] calcs = new Calculator[0];
+
+        /// <summary>
+        /// Форма с историей вычислений
+        /// </summary>
+        private readonly HistoryForm HF;
+
+        /// <summary>
+        /// Форма отображения памяти калькулятора
+        /// </summary>
+        private readonly MRForm mrForm;
+
         public MainWindow()
         {
             InitializeComponent();
             mrForm = new MRForm(this);
             HF = new HistoryForm(this);
+        }
+
+        private void Number_Click(object sender, RoutedEventArgs e)
+        {
+            inputVal(((Button)sender).Content.ToString()[0]);
+        }
+
+        /// <summary>
+        /// Ввод символа
+        /// </summary>
+        /// <param name="c">Вводимый символ</param>
+        private void inputVal(char c)
+        {
+            label1.Content = calc.inputValues(c);
+            setTextSize();
+        }
+
+        private void btn_Zero_Click(object sender, RoutedEventArgs e)
+        {
+            if (calc.arg == "") typeZeroComa();
+            else label1.Content = calc.inputValues('0');
+        }
+
+        private void btn_clear_Click(object sender, RoutedEventArgs e)
+        {
+            resetCalc();
+            Array.Clear(calc.args, 0, 1);
+            label1.Content = "0";
+        }
+
+        private void btn_bspace_Click(object sender, RoutedEventArgs e)
+        {
+            label1.Content = calc.deleteSymbol();
+            setTextSize();
+        }
+
+        private void btn_Negative_Click(object sender, RoutedEventArgs e)
+        {
+            calc.minus = !calc.minus;
+            inputVal('-');
+        }
+
+        private void btn_Coma_Click(object sender, RoutedEventArgs e)
+        {
+            if (calc.arg.Contains(',') == false)
+            {
+                if (calc.arg == "")
+                {
+                    typeZeroComa();
+                }
+                else label1.Content = calc.inputValues(',');
+            }
+        }
+
+        /// <summary>
+        /// Ввод "0,"
+        /// </summary>
+        public void typeZeroComa()
+        {
+            label1.Content = calc.inputValues('0');
+            label1.Content = calc.inputValues(',');
         }
 
         public void setTextSize()
@@ -61,6 +142,362 @@ namespace WpfTestTask
                     label1.FontSize=fSize;
                     s = fSize * str.Length * x;
                 }
+        }
+
+        /// <summary>
+        /// Нажатие одной из функций
+        /// </summary>
+        /// <param name="f">Делегат функции выполнения</param>
+        /// <param name="isExtraFunc">Флаг функции одного аргумента</param>
+        private void btn_Func_Click(Calculator.funcDeleg f, bool isExtraFunc)
+        {
+            if (isExtraFunc == true)
+            {
+                calc.extraFunc(f);
+                label1.Content = calc.disp;
+                saveStatus();
+                setTextSize();
+            }
+            else funcClick(f);
+        }
+
+        private void btn_plus_Click(object sender, RoutedEventArgs e)
+        {
+            btn_click("+", new Calculator.CalcFunction().Summ, false);
+        }
+
+        private void btn_multiply_Click(object sender, RoutedEventArgs e)
+        {
+            btn_click("×", new Calculator.CalcFunction().multiply, false);
+        }
+
+        private void btn_divide_Click(object sender, RoutedEventArgs e)
+        {
+            btn_click("÷", new Calculator.CalcFunction().divide, false);
+        }
+
+        private void btn_minus_Click(object sender, RoutedEventArgs e)
+        {
+            btn_click("-", new Calculator.CalcFunction().differens, false);
+        }
+
+        private void btn_SQRT_Click(object sender, RoutedEventArgs e)
+        {
+            calc.symbol = "√";
+            calc.fDeleg = new Calculator.CalcFunction().sqrtOf;
+            btn_Func_Click(calc.fDeleg, true);
+        }
+
+        private void btn_SQR_Click(object sender, RoutedEventArgs e)
+        {
+            calc.symbol = "^";
+            calc.fDeleg = new Calculator.CalcFunction().sqrOf;
+            btn_Func_Click(calc.fDeleg, true);
+        }
+
+        private void btn_Fraction_Click(object sender, RoutedEventArgs e)
+        {
+            calc.symbol = "1/";
+            calc.fDeleg = new Calculator.CalcFunction().fraction;
+            btn_Func_Click(calc.fDeleg, true);
+        }
+
+        private void btn_Percent_Click(object sender, EventArgs e)
+        {
+            string location = Directory.GetCurrentDirectory() + "/calc.json";
+            string[] s = File.ReadAllLines(location);
+            int i = 0;
+            foreach (string str in s)
+            {
+                Array.Resize(ref calcs, i + 1);
+                calcs[i] = JsonConvert.DeserializeObject<Calculator>(str);
+                hf.HistoryList.Items.Add(calcs[i].resultString);
+                i++;
+            }
+
+            hf.Left = Left + (Width - hf.Width) / 2;
+            hf.Top = Top + (Height - hf.Height) / 2;
+            Enabled = false;
+            hf.Show();
+        }
+
+        /// <summary>
+        /// Выполнение заданной функции
+        /// </summary>
+        /// <param name="f">Делегат заданной функции</param>
+        private void funcClick(Calculator.funcDeleg f)
+        {
+            calc.resBtnFlag = false;
+
+            switch (calc.index)
+            {
+                case false:
+                    {
+                        calc.fDeleg = f;
+                        calc.tryToGetArg(calc.arg);
+                        calc.funcFlag = true;
+                        break;
+                    }
+                case true:
+                    {
+                        calc.tryToGetArg(calc.arg); ;
+                        getResult(calc.fDeleg);
+                        calc.funcFlag = true;
+                        calc.fDeleg = f;
+                        break;
+                    }
+            }
+
+            label1.Content = calc.disp;
+            calc.minus = false;
+        }
+
+        private void btn_click(string s, Calculator.funcDeleg f, bool isExtraFunc)
+        {
+            calc.symbol = s;
+
+            if (calc.funcFlag == true)
+            {
+                calc.fDeleg = f;
+                calc.resBtnFlag = false;
+            }
+            else
+            {
+                btn_Func_Click(f, isExtraFunc);
+                setTextSize();
+            }
+        }
+
+        private void btn_Result_Click(object sender, RoutedEventArgs e)
+        {
+            calc.tryToGetArg(calc.arg);
+
+            calc.resBtnFlag = true;
+            calc.funcFlag = true;
+
+            calc.getResult(calc.fDeleg);
+
+            label1.Content = calc.disp;
+
+            saveStatus();
+
+            setTextSize();
+        }
+
+        private void btn_MR_Click(object sender, RoutedEventArgs e)
+        {
+            getFromMR(calc.mr.Length - 1);
+            label1.Content = calc.arg;
+
+            calc.mrFlag = true;
+        }
+
+        private void btn_MPlus_Click(object sender, RoutedEventArgs e)
+        {
+            setMR(calc.mr.Length - 1, 1);
+
+            calc.mrFlag = true;
+
+            btn_MC.Enabled = true;
+            btn_MR.Enabled = true;
+        }
+
+        private void btn_MC_Click(object sender, RoutedEventArgs e)
+        {
+            calc.mr = new double[1];
+            btn_MList.Enabled = false;
+            mf.listBox_MR.Items.Clear();
+            switchMRButtons();
+        }
+
+        private void btn_MS_Click(object sender, RoutedEventArgs e)
+        {
+            int l = calc.mr.Length - 1;
+
+            if (calc.mr.Length > 0)
+            {
+                Array.Resize(ref calc.mr, l + 2);
+                l++;
+            }
+            setMR(l, 1);
+
+            calc.mrFlag = true;
+
+            btn_MC.Enabled = true;
+            btn_MR.Enabled = true;
+        }
+
+        private void btn_MList_Click(object sender, RoutedEventArgs e)
+        {
+            mf.Left = Left + (Width - hf.Width) / 2;
+            mf.Top = Top + (Height - hf.Height) / 2;
+            mf.Show();
+            Enabled = false;
+        }
+
+        private void btn_MMinus_Click(object sender, RoutedEventArgs e)
+        {
+            setMR(calc.mr.Length - 1, -1);
+            calc.mrFlag = true;
+
+            btn_MC.Enabled = true;
+            btn_MR.Enabled = true;
+        }
+
+        public void setMR(int indexOf, int negative)
+        {
+            try
+            {
+                calc.mr[indexOf] += Convert.ToDouble(calc.arg) * negative;
+            }
+            catch
+            {
+                calc.mr[indexOf] = calc.args[0];
+                calc.arg = calc.mr[indexOf].ToString();
+            }
+
+            setMrList(indexOf);
+
+            calc.funcFlag = true;
+            calc.resBtnFlag = true;
+
+            btn_MList.Enabled = true;
+
+            calc.mrFlag = true;
+        }
+
+        public void setMrList(int indexOf)
+        {
+            try
+            {
+                mf.listBox_MR.Items[calc.mr.Length - 1] = calc.mr[indexOf];
+            }
+            catch
+            {
+                mf.listBox_MR.Items.Add(calc.mr[indexOf]);
+            }
+        }
+
+        public void switchMRButtons()
+        {
+            btn_MR.Enabled = false;
+            btn_MC.Enabled = false;
+        }
+
+        /// <summary>
+        /// Получение аргумента из памяти
+        /// </summary>
+        /// <param name="indexOf">индекс аргумента в массиве памяти</param>
+        public void getFromMR(int indexOf)
+        {
+            calc.arg = calc.mr[indexOf - 1].ToString();
+            if (calc.mr[indexOf - 1] < 0) calc.minus = true;
+            calc.disp = calc.displayOut(calc.arg);
+
+            label1.Content = calc.disp;
+        }
+
+        private void listBox_MR_DoubleClick(object sender, EventArgs e)
+        {
+            getFromMR(mf.listBox_MR.SelectedIndex + 1);
+            calc.resBtnFlag = true;
+        }
+
+        /// <summary>
+        /// Инициатор сохранения статуса калькулятора
+        /// </summary>
+        public void saveMe()
+        {
+            addCalc();
+            try
+            {
+                saveCalc();
+            }
+            catch { this.Content = "Не удалось сохранить в файл"; }
+        }
+
+        /// <summary>
+        /// Загрузка калькулятора
+        /// </summary>
+        /// <param name="i">Индекс загружаемого калькулятора из массива калькуляторов></param>
+        public void loadMe(int i)
+        {
+            calc = new Calculator(calcs[i]);
+            calc.resultString = "";
+
+            label1.Content = calc.displayOut(calc.disp);
+
+            resetCalc();
+
+            calc.arg = calc.args[0].ToString();
+        }
+
+        /// <summary>
+        /// Инициатор сброса калькулятора
+        /// </summary>
+        public void resetCalc()
+        {
+            calc.ResetCalc();
+            label1.FontSize = 30;
+        }
+
+        /// <summary>
+        /// Пополнение массива состояний новым состоянием калькулятора
+        /// </summary>
+        private void addCalc()
+        {
+            int i = calcs.Length;
+            Array.Resize(ref calcs, i + 1);
+            calcs[calcs.Length - 1] = new Calculator(calc);
+        }
+
+        /// <summary>
+        /// Сохранение состояния калькулятора в файл
+        /// </summary>
+        private void saveCalc()
+        {
+            Calculator c = new Calculator(calc);
+
+            c.fDeleg = null;
+
+            string s = JsonConvert.SerializeObject(c);
+
+            string location = Directory.GetCurrentDirectory() + "/calc.json";
+            File.AppendAllText(location, s + "\n");
+        }
+
+        /// <summary>
+        /// Добавление в список вычисления целиком
+        /// </summary>
+        /// <param name="c">Калькулятор</param>
+        private void addToCalcList(Calculator c)
+        {
+            Combobox1.Items.Add(c.resultString);
+        }
+
+        /// <summary>
+        /// Инициатор получения результата
+        /// </summary>
+        /// <param name="cf">Делегат функции</param>
+        private void getResult(Calculator.funcDeleg cf)
+        {
+            calc.getResult(cf);
+            saveStatus();
+        }
+
+        /// <summary>
+        /// Фиксация этапа вычисления
+        /// </summary>
+        private void saveStatus()
+        {
+            saveMe();
+            if (calc.resultString != "") addToCalcList(calc);
+            calc.resultString = "";
+        }
+
+        private void Combobox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            loadMe(Combobox1.SelectedIndex);
         }
 
         private void setFonSize(float fSize)
@@ -98,14 +535,25 @@ namespace WpfTestTask
             btn_CE.FontSize = fSize;
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
         {
+            setTextSize();
 
-        }
+            bool flag = sizeFlag;
 
-        private void gridSplitter_DragDelta(object sender, System.Windows.Controls.Primitives.DragDeltaEventArgs e)
-        {
+            if (Height >= 400) setTextSize();
 
+            if (Height > 650 && Width > 350) sizeFlag = true;
+            else sizeFlag = false;
+
+            if (flag != sizeFlag)
+            {
+                switch (sizeFlag)
+                {
+                    case true: setFonSize(20); break;
+                    case false: setFonSize(12); break;
+                }
+            }
         }
 
         private void btn_M_Click(object sender, RoutedEventArgs e)
@@ -132,27 +580,6 @@ namespace WpfTestTask
             IsEnabled = true;
             f.Visibility = Visibility.Collapsed;
             e.Cancel = true;
-        }
-
-        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            setTextSize();
-
-            bool flag = sizeFlag;
-
-            if (Height >= 400) setTextSize();
-
-            if (Height > 650 && Width > 350) sizeFlag = true;
-            else sizeFlag = false;
-
-            if (flag != sizeFlag)
-            {
-                switch (sizeFlag)
-                {
-                    case true: setFonSize(20); break;
-                    case false: setFonSize(12); break;
-                }
-            }
         }
     }
 }
